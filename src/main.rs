801d6e65cbdf2835b42472ecf1980cd7ffd56aa3
Anton Sidorov <antosid@gmail.com>
2024-07-20 16:00:09 +0100

    next:

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use std::time::Duration;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 #[clap(
-    version = "v0.2.10",
+    version = "v0.3.0",
     author = "Anton Sidorov tonysidrock@gmail.com",
     about = "ADS-B squitter decoder"
 )]
