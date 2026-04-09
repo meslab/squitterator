b3cafbe8efd61b78830f100883758bf312d6dbe6
The Stronghold of Two <86304474+dezerdigah-duo@users.noreply.github.com>
2026-04-09 09:28:36 +0200

    alternative-buffer: (#95)
    Co-authored-by: Tony Sidrock <tonysidrock@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,21 @@ use std::sync::Arc;
 
 fn main() -> AppResult<()> {
     let args = Arc::new(Args::parse());
+    ctrlc::set_handler(move || {
+        print!("\x1B[0m\x1B[?25h");
+        print!("\x1b[?1049l");
+        std::io::Write::flush(&mut std::io::stdout()).unwrap();
+
+        std::process::exit(0);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    std::panic::set_hook(Box::new(|info| {
+        // Reset terminal before printing the panic message
+        print!("\x1b[?1049l");
+        eprintln!("Panic occurred: {:?}", info);
+    }));
+    print!("\x1b[?1049h");
 
     // Initialize error logging if specified
     if let Some(error_log_file) = &args.error_log {
